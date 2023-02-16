a3f4158ce5c55ec14f097dbe790f00c5d17da7a9
Lyeeroy <32140991+Lyeeroy@users.noreply.github.com>
2023-02-16 14:05:55 +0100

    Update TAP: TikTok Auto Play.js

--- TAP: TikTok Auto Play.js
+++ TAP: TikTok Auto Play.js
@@ -1,17 +1,16 @@
 // ==UserScript==
 // @name         TAP: TikTok Auto Play
-// @namespace    https://github.com/Lyeeroy/tiktok-autoplay
+// @namespace    http://tampermonkey.net/
 // @version      0.2
 // @description  Automatically plays the next TikTok video
 // @author       LyeeRoy
 // @match        https://www.tiktok.com/*
 // @icon         https://cdn4.iconfinder.com/data/icons/social-media-flat-7/64/Social-media_Tiktok-512.png
-// @updateURL    https://github.com/Lyeeroy/tiktok-autoplay/blob/main/TAP:%20TikTok%20Auto%20Play.js
-// @downloadURL  https://github.com/Lyeeroy/tiktok-autoplay/blob/main/TAP:%20TikTok%20Auto%20Play.js
 // @grant        none
 // ==/UserScript==
 
 // If you pause the video, minimaze or somehow stop the video you'll need to manually move on to the next one to continue.
+
 var autoplay = true; // To disable Autoplay as the default setting, set the value to FALSE.
 var hideChat = true; // To keep the "Hide CHAT" button invisible, set the value to FALSE.
 
@@ -48,11 +47,6 @@ var hideChat = true; // To keep the "Hide CHAT" button invisible, set the value 
                                 button.click();
                             }
                         }
-                    } else {
-                        var button = document.evaluate('/html/body/div[2]/div[2]/div[3]/div[1]/button[3]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-                        if (button && !button.innerHTML.includes('Show CHAT')) {
-                            button.innerHTML = 'Show CHAT';
-                        }
                     }
                 }
             }, 50);
@@ -86,13 +80,10 @@ var hideChat = true; // To keep the "Hide CHAT" button invisible, set the value 
                     btn.style.border = "2px solid #808080";
                 }
             });
-
-            if (!window.location.href.startsWith('https://www.tiktok.com/@')) {
-                btn.style.display = 'none';
-            }
         }
     }, 50);
 })();
+
 
 (function() {
     if (hideChat) {
@@ -102,49 +93,28 @@ var hideChat = true; // To keep the "Hide CHAT" button invisible, set the value 
         const elementClass = '.tiktok-3q30id-DivContentContainer';
 
         // Create the button
+        const button = document.createElement('button');
+        button.innerHTML = 'Hide CHAT';
+        button.style.position = "fixed";
+        button.style.bottom = "70px";
+        button.style.left = "20px";
+        button.style.zIndex = "9999";
+        button.style.backgroundColor = "transparent";
+        button.style.color = "#696969";
+        button.style.padding = "10px 20px";
+        button.style.border = "2px solid #808080";
+        button.style.borderRadius = "5px";
+        button.style.cursor = "pointer";
 
-(function() {
-  if (hideChat) {
-    'use strict';
+        // Add the button to the page
+        document.body.appendChild(button);
 
-    // Specify the class name of the element to hide
-    const elementClass = '.tiktok-3q30id-DivContentContainer';
-
-    // Create the button
-    const button = document.createElement('button');
-    button.innerHTML = 'Hide CHAT';
-    button.style.position = "fixed";
-    button.style.bottom = "70px";
-    button.style.left = "20px";
-    button.style.zIndex = "9999";
-    button.style.backgroundColor = "transparent";
-    button.style.color = "#696969";
-    button.style.padding = "10px 20px";
-    button.style.border = "2px solid #808080";
-    button.style.borderRadius = "5px";
-    button.style.cursor = "pointer";
-
-    // Add the button to the page
-    document.body.appendChild(button);
-
-    // Add a click event to the button that hides or shows the element
-    button.addEventListener('click', function() {
-      const elements = document.querySelectorAll(elementClass);
-      for (const element of elements) {
-        if (element.style.display === 'none') {
-          element.style.display = 'block';
-          button.innerHTML = 'Hide CHAT';
-        } else {
-          element.style.display = 'none';
-          button.innerHTML = 'Show CHAT';
-        }
-      }
-    });
-
-    // Check if URL starts with "https://www.tiktok.com/@", otherwise hide the button
-    const currentUrl = window.location.href;
-    if (!currentUrl.startsWith('https://www.tiktok.com/@')) {
-      button.style.display = 'none';
+        // Add a click event to the button that hides the element
+        button.addEventListener('click', function() {
+            const elements = document.querySelectorAll(elementClass);
+            for (const element of elements) {
+                element.style.display = 'none';
+            }
+        });
     }
-  }
 })();
